a2d381d6684b2e7b55dce4752945eeac7a32dad6
AL0482 <ania.koszalka@gmail.com>
2024-03-18 12:51:40 +0100

    2 commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,20 @@
-import React from "react";
-import { pageLinks, socialLinks } from "../data";
-
+import { pageLinks, socialLinks } from '../data'
+import PageLinks from './PageLinks'
+import SocialLink from './SocialLink'
 const Footer = () => {
   return (
-    <footer className="section footer">
-      <ul className="footer-links">
-        {pageLinks.map((link) => {
-          const { id, href, text } = link;
-          return (
-            <li key={id}>
-              <a href={href} className="footer-link">
-                {text}
-              </a>
-            </li>
-          );
+    <footer className='section footer'>
+      <PageLinks parentClass='footer-links' itemClass='footer-link' />
+      <ul className='footer-icons'>
+        {socialLinks.map((link) => {
+          return <SocialLink key={link.id} {...link} itemClass='footer-icon' />
         })}
       </ul>
-      <ul className="footer-icons">
-        {socialLinks.map((sLink) => {
-          return (
-            <li key={sLink.id}>
-              <a href={sLink.href} target="_blank" className="footer-icon">
-                <i className={sLink.icon}></i>
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-      <p className="copyright">
+      <p className='copyright'>
         copyright &copy; Backroads travel tours company
-        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+        <span id='date'>{new Date().getFullYear()}</span>. all rights reserved
       </p>
     </footer>
-  );
-};
-
-export default Footer;
+  )
+}
+export default Footer
